c8045f5e8a2b47b3fea37a2821d8fa4f446064c6
Bhupendra Singh <bhsingh@gmail.com>
2016-11-29 11:36:32 +0530

    moved app code to javascript file.

---
+++ gps.js
@@ -0,0 +1,33 @@
+var APP = (function () {
+
+var PRE = React.createFactory('pre');
+var DIV = React.createFactory('div');
+
+
+var JSONView = React.createFactory(React.createClass({
+    render: function() {
+        return PRE(null,
+                   JSON.stringify(this.props.obj, null, 4));
+    }
+}));
+
+var App = React.createFactory(React.createClass({
+    render: function() {
+        return DIV(null,
+                   JSONView({obj: this.props.coords}, null));
+    }
+}));
+
+return function () {
+    navigator.geolocation.getCurrentPosition(function (pos) {
+        var coords = {
+            latitude: pos.coords.latitude,
+            longitude: pos.coords.longitude,
+            accuracy: pos.coords.accuracy};
+
+        ReactDOM.render(App({coords: coords}, null),
+                        document.getElementById('app'));
+    });
+}
+
+})();
