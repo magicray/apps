597bf4c0ddf5b2af8d9b1cff02e1cc82e3114b06
Bhupendra Singh <bhsingh@gmail.com>
2016-12-07 18:20:19 +0530

    Added toggle button for location/address.

--- gps.js
+++ gps.js
@@ -7,31 +7,56 @@ var BUTTON = React.createFactory('button');
 
 return React.createFactory(React.createClass({
     getInitialState: function() {
-        return {obj: this.props.obj}
+        return {val: this.props.val, loc: '', action: 'Show Location'}
     },
 
-    componentDidMount: function () {
-        this.refresh();
+    componentDidMount: function() {
+        this.showLocation();
     },
 
-    refresh: function() {
+
+    showAddress: function() {
         var that = this;
 
-        that.setState({obj: 'Refreshing..'});
+        var maps = 'http://maps.googleapis.com/maps/api/geocode/json?';
+        var latlng = this.state.loc.latitude + ',' + this.state.loc.longitude;
+        var url = maps + 'latlng=' + latlng;
+
+        axios.get(url).then(function(response) {
+            that.setState({action: 'Show Location',
+                val: response.data.results[0]['formatted_address'].replace(
+                    /, */g, '\n')});
+        }).catch(function(error){
+            alert(error);
+        })
+    },
+
+    onClick: function() {
+        if ('Show Location' === this.state.action) {
+            this.showLocation();
+        } else {
+            this.showAddress();
+        }
+    },
+
+    showLocation: function () {
+        var that = this;
 
         navigator.geolocation.getCurrentPosition(function (pos) {
-            that.setState({obj: {
-                latitude: pos.coords.latitude,
-                longitude: pos.coords.longitude,
-                accuracy: pos.coords.accuracy}});
+            loc = pos.coords;
+            that.setState({
+                loc: loc,
+                action: 'Show Address',
+                val: JSON.stringify({
+                    latitude: loc.latitude,
+                    longitude: loc.longitude,
+                    accuracy: loc.accuracy}, null, 4)});
         });
     },
 
     render: function() {
-        var text = JSON.stringify(this.state.obj, null, 4);
-
         return DIV({className: 'col-sm-2'},
-                   PRE(null, text),
-                   BUTTON({onClick: this.refresh}, 'Refresh'));
+                   PRE(null, this.state.val),
+                   BUTTON({onClick: this.onClick}, this.state.action));
     }}));
 })();
