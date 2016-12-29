6a53f14d72544c974d34a5844fc3bfdeee4eecdf
Bhupendra Singh <bhsingh@gmail.com>
2016-12-29 22:01:45 +0530

    Fixed a typo

--- external.sh
+++ external.sh
@@ -8,19 +8,19 @@ JS="react/15.4.1/react                            \
     react-router/4.0.0-0/react-router             \
     redux/3.6.0/redux                             \
     react-redux/4.4.6/react-redux                 \
-    lodash/4.17.3/loadash                         \
+    lodash.js/4.17.3/lodash                       \
     immutable/3.8.1/immutable"
 
 rm -f min.js js external.min.js external.js
 
 for js in $JS; do
-    curl $CDN/$js.min.js >> min.js
+    curl -f $CDN/$js.min.js >> min.js
+    curl -f $CDN/$js.js >> js
     echo >> min.js
-    curl $CDN/$js.js >> js
 done
 
 mv min.js external.min.js
 mv js external.js
 
-curl https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css > css
+curl -f https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css > css
 mv css bootstrap.css
