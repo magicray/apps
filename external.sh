d8682571584130ea933a6b9e688b611e863eab7d
Bhupendra Singh <bhsingh@gmail.com>
2016-12-29 21:31:42 +0530

    combining external javascript libraries together.

---
+++ external.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+CDN=https://cdnjs.cloudflare.com/ajax/libs
+
+JS="react/15.4.1/react                            \
+    react/15.4.1/react-dom                        \
+    react-bootstrap/0.30.7/react-bootstrap        \
+    react-router/4.0.0-0/react-router             \
+    redux/3.6.0/redux                             \
+    react-redux/4.4.6/react-redux"
+
+rm -f min.js js external.min.js external.js
+
+for js in $JS; do
+    curl $CDN/$js.min.js >> min.js
+    echo >> min.js
+    curl $CDN/$js.js >> js
+done
+
+mv min.js external.min.js
+mv js external.js
+
+curl https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css > css
+mv css bootstrap.css
