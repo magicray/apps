96666147175bbdb397bcc0fbafb4ab19438b06f4
Bhupendra Singh <bhsingh@gmail.com>
2017-01-05 09:54:46 +0530

    auto div creation for top level apps

--- react-app.js
+++ react-app.js
@@ -3,20 +3,27 @@ class App extends React.Component {
         super(props)
 
         this.href = document.createElement('a')
-        this.href = window.location
+
+        if(props.div === undefined) {
+            this.href = window.location
+        }
 
         this.actions = {
             changeView: (location) => {
                 this.href = location
-                console.log('changeView', location, this.state)
+
+                if(props.div === undefined) {
+                    window.location = this.href
+                }
+
                 this.setState(this.state)
             }
         }
+
         this.state = JSON.parse(JSON.stringify(props.state))
 
         const setState = (state) => this.setState(state)
-        const getState = () => JSON.parse(JSON.stringify(this.state))
-        const m = ['PromisePending', 'PromiseResolved', 'PromiseRejected']
+        const getState = () => this.state
         
         for(let k in this.props.reducers) {
             if(!this.props.reducers.hasOwnProperty(k))
@@ -24,37 +31,37 @@ class App extends React.Component {
 
             const s = k.split('_')
             
-            if(m.indexOf(s.pop()) > -1 && s !== k) {
+            if('PromisePending' === s.pop() && s !== k) {
                 k = s.join('_')
 
-                const pending = this.props.reducers[k + '_' + m[0]]
-                const resolved = this.props.reducers[k + '_' + m[1]]
-                const rejected = this.props.reducers[k + '_' + m[2]]
+                const pending = this.props.reducers[k + '_PromisePending']
+                const resolved = this.props.reducers[k + '_PromiseResolved']
+                const rejected = this.props.reducers[k + '_PromiseRejected']
                 
                 this.actions[k] = function(promise) {
                     const state = getState()
+
                     try {
-                        console.log(pending.name, null, state)
                         pending(state)
                         setState(state)
 
                         promise.then(r => {    
                             const state = getState()
+
                             try {
-                                console.log(resolved.name, r, state)
                                 resolved(state, r)
                                 setState(state)
                             } catch(e) {
-                                console.log(e)
+                                console.log(resolved.name, state, e)
                             }
                         }).catch(e => {
                             const state = getState()
+
                             try {
-                                console.log(rejected.name, e, state)
                                 rejected(state, e)
                                 setState(state)
                             } catch(e) {
-                                console.log(e)
+                                console.log(rejected.name, state, e)
                             }
                         })
                     } catch(e) {
@@ -68,11 +75,10 @@ class App extends React.Component {
                     const state = getState()
                     
                     try {
-                        console.log(reducer.name, arguments, state)
                         reducer(state, ...arguments)
                         setState(state)
                     } catch(e) {
-                        console.log(e)
+                        console.log(reducer.name, arguments, state, e)
                     }
                 }
             }
@@ -84,14 +90,23 @@ class App extends React.Component {
     render() {
         const component = this.props.views[this.href.hash] || this.props.home
 
-        console.log(this.href.hash, this.state)
         return React.createElement(component, {
                 actions: this.actions,
-                state: JSON.parse(JSON.stringify(this.state))
+                state: this.state
             })
     }
 
-    static stringify(obj) {
-        return JSON.stringify(obj, null, 4)
+    static mount(props) {
+        let div = ''
+
+        if(props.div === undefined) {
+            div = document.createElement('div')
+            document.body.appendChild(div)
+            div.id = Date.now()
+        } else {
+            div = document.getElementById(props.div)
+        }
+
+        ReactDOM.render(React.createElement(App, props), div)
     }
 }
