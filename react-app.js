e2de2095f2f419a205f37ca2e1501df3b9cd1cb5
Bhupendra Singh <bhsingh@gmail.com>
2017-01-15 20:07:21 +0530

    more refactoring..

--- react-app.js
+++ react-app.js
@@ -1,88 +1,87 @@
-class AppClass extends React.Component {
-    constructor(props) {
-        super(props)
+function App(state) {
+    class Component extends React.Component {
+        constructor(props) {
+            super(props)
 
-        const getState = () => this.state
-        const setState = (state) => this.setState(state)
-        const getAction = (action) => this.props.actions[action]
+            const getState = () => this.state
+            const setState = (state) => this.setState(state)
+            const getAction = (action) => this.props.actions[action]
 
-        this.state = {
-            view: () => React.DOM.h1(null, 'Loading'),
-            state: JSON.parse(JSON.stringify(props.state))
-        }
+            this.state = {
+                view: () => React.DOM.h1(null, 'Loading'),
+                state: JSON.parse(JSON.stringify(props.state))
+            }
 
-        this.actions = {}
+            this.actions = {}
 
-        for(let k in this.props.actions) {
-            const reducer = this.props.actions[k]
+            for(let k in this.props.actions) {
+                const reducer = this.props.actions[k]
 
-            this.actions[k] = function() {
-                const state = getState()
+                this.actions[k] = function() {
+                    const state = getState()
 
-                try {
-                    const promise = reducer.apply(state, arguments)
-                    setState(state)
+                    try {
+                        const promise = reducer.apply(state, arguments)
+                        setState(state)
 
-                    if(promise) {
-                        promise.then(val => {
-                            const state = getState()
-                            getAction(k + '_PromiseResolved').apply(
-                                state, [val])
-                            setState(state)
-                        }).catch(val => {
-                            const state = getState()
-                            getAction(k + '_PromiseRejected').apply(
-                                state, [val])
-                            setState(state)
-                        })
+                        if(promise) {
+                            promise.then(val => {
+                                const state = getState()
+                                getAction(k + '_PromiseResolved').apply(
+                                    state, [val])
+                                setState(state)
+                            }).catch(val => {
+                                const state = getState()
+                                getAction(k + '_PromiseRejected').apply(
+                                    state, [val])
+                                setState(state)
+                            })
+                        }
+                    } catch(e) {
+                        console.log(reducer.name, arguments, state, e)
                     }
-                } catch(e) {
-                    console.log(reducer.name, arguments, state, e)
                 }
             }
+
+            console.log('initialized')
         }
 
-        console.log('initialized')
-    }
+        componentDidMount() {
+            const a = document.createElement('a')
 
-    componentDidMount() {
-        const a = document.createElement('a')
-
-        window.onhashchange = () => {
-            a.href = window.location
-            this.actions[a.hash.slice(2)]()
-        }
-
-        a.href = window.location
-        const action = a.hash? a.hash.slice(2): this.props.onload
-        this.actions[action]()
-    }
-
-    render() {
-        return React.createElement(this.state.view, {
-            state: this.state.state,
-            actions: this.actions})
-    }
-}
-
-function App(state) {
-    return {
-        render: function(props) {
-            return React.createElement(AppClass, props)
-        },
-
-        mount: function(onload, divId) {
-            let div = document.getElementById(divId)
-
-            if(undefined === divId) {
-                div = document.createElement('div')
-                document.body.appendChild(div)
+            window.onhashchange = () => {
+                a.href = window.location
+                this.actions[a.hash.slice(2)]()
             }
 
-            ReactDOM.render(
-                React.createElement(this.render, {
-                    state, onload, actions: this}),
-                div)
+            a.href = window.location
+            const action = a.hash? a.hash.slice(2): this.props.onload
+            this.actions[action]()
+        }
+
+        render() {
+            return React.createElement(this.state.view, {
+                state: this.state.state,
+                actions: this.actions})
         }
     }
+
+    function render(props) {
+        return React.createElement(Component, props)
+    }
+
+    function mount(onload, divId) {
+        let div = document.getElementById(divId)
+
+        if(undefined === divId) {
+            div = document.createElement('div')
+            document.body.appendChild(div)
+        }
+
+        ReactDOM.render(
+            React.createElement(render, {state, onload, actions: this}),
+            div)
+    }
+
+    return {render, mount}
 }
